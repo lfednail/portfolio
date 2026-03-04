7c9afe7ba5492f7bc41dbba786ee56e0fdcb70c7
lfednail <lfednail@gmail.com>
2026-03-04 10:11:49 +0100

    `Updated dependencies and configurations in package.json, pnpm-lock.yaml, and tsconfig.json files`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,51 +1,56 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider } from "@/components/theme-provider";
 import SiteHeader from "@/components/navigation/site-header";
 import SiteFooter from "@/components/navigation/site-footer";
-import React from "react";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
+  display: "swap",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
+  display: "swap",
 });
 
 export const metadata = {
   title: "Fednail-LL - Portfolio",
   description:
-    "Portfolio de FEDNAIL LECLERCQ -  Développeur informatique, je suis un développeur web et mobile, j'aime le développement web, le développement mobile et le développement de jeux vidéo.",
+    "Portfolio de FEDNAIL LECLERCQ - Développeur informatique, je suis un développeur web et mobile, j'aime le développement web, le développement mobile et le développement de jeux vidéo.",
 };
-export default function RootLayout({children,}: Readonly<{
+
+export default function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-    <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      suppressHydrationWarning
-    >
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <SiteHeader />
-      <main>{children}</main>
-      <SiteFooter />
-    </ThemeProvider>
-    
-    <SpeedInsights />
-    <Analytics />
-    </body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SiteHeader />
+          <main>{children}</main>
+          <SiteFooter />
+        </ThemeProvider>
+
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   );
 }
